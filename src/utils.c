f6a0b2cbd32c9c98eaf6aca2c1fd7bbcc255d5a1
高辛格 <xg.gao@tianrang-inc.com>
2019-09-17 10:56:47 +0800

    code clean

--- src/utils.c
+++ src/utils.c
@@ -8,24 +8,9 @@
 #include <limits.h>
 #include <time.h>
 #include <sys/time.h>
-
 #include "utils.h"
 
-
-/*
-// old timing. is it better? who knows!!
-double get_wall_time()
-{
-    struct timeval time;
-    if (gettimeofday(&time,NULL)){
-        return 0;
-    }
-    return (double)time.tv_sec + (double)time.tv_usec * .000001;
-}
-*/
-
-double what_time_is_it_now()
-{
+double what_time_is_it_now() {
     struct timeval time;
     if (gettimeofday(&time,NULL)){
         return 0;
